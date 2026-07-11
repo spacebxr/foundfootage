de2d425389c8dda07f99989a9620e9b98fe2a72f
spacebxr <spacebxr112@gmail.com>
2026-07-11 18:39:57 +0530

    first commit

--- src/main/java/com/sp/render/gui/TitleText.java
+++ src/main/java/com/sp/render/gui/TitleText.java
@@ -10,36 +10,36 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 public class TitleText implements HudRenderCallback {
-    public TitleText (){
+    public TitleText() {
     }
 
     @Override
     public void onHudRender(DrawContext drawContext, float tickDelta) {
         CutsceneManager cutsceneManager = SPBRevampedClient.getCutsceneManager();
 
-        if(cutsceneManager.backroomsBySP && !cutsceneManager.blackScreen.isBlackScreen){
+        if (cutsceneManager.backroomsBySP && !cutsceneManager.blackScreen.isBlackScreen) {
             Text text = Text.translatable("intro.backrooms");
-            Text text2 = Text.translatable("intro.credit").append(Text.literal("SpacePotato").formatted(Formatting.GOLD));
+            Text text2 = Text.translatable("intro.credit").append(Text.literal("Spacebxr").formatted(Formatting.GOLD));
             MinecraftClient client = MinecraftClient.getInstance();
-            if(client != null){
+            if (client != null) {
                 int width = client.getWindow().getScaledWidth();
                 int height = (client.getWindow().getScaledHeight());
 
                 drawContext.getMatrices().push();
-                drawContext.getMatrices().translate((float)(width / 2), (float)(height / 2), 0.0F);
+                drawContext.getMatrices().translate((float) (width / 2), (float) (height / 2), 0.0F);
                 RenderSystem.enableBlend();
                 drawContext.getMatrices().push();
                 drawContext.getMatrices().scale(2.5F, 2.5F, 2.5F);
                 int w = client.textRenderer.getWidth(text);
                 int h = client.textRenderer.fontHeight;
-                drawContext.drawText(client.textRenderer, text, -w/2,  -h/2, 0xFFFFFF, true);
+                drawContext.drawText(client.textRenderer, text, -w / 2, -h / 2, 0xFFFFFF, true);
                 drawContext.getMatrices().pop();
 
                 drawContext.getMatrices().push();
                 drawContext.getMatrices().scale(1.0F, 1.0F, 1.0F);
                 int w2 = client.textRenderer.getWidth(text2);
                 int h2 = client.textRenderer.fontHeight;
-                drawContext.drawText(client.textRenderer, text2, -w2/2,  10, 0xFFFFFF, true);
+                drawContext.drawText(client.textRenderer, text2, -w2 / 2, 10, 0xFFFFFF, true);
                 drawContext.getMatrices().pop();
 
                 RenderSystem.disableBlend();
